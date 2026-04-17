67503c77868bf0d9e9042de576be988be1c2aa84
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-17 09:00:39 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,15 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-  let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
-
-if (currentLink) {
-  // or if (currentLink !== undefined)
-  currentLink.classList.add('current');
-}
-
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
@@ -26,8 +17,19 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   // next step: create link and add it to nav
+
+  // Create link and add it to nav
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
-// Create link and add it to nav
-nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
+}
+
+
 
