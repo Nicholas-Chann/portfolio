b13a215b59b999283161f580201b288efbe58938
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-17 10:30:33 -0700

    Update global.js

--- global.js
+++ global.js
@@ -54,7 +54,7 @@ document.body.insertAdjacentHTML(
   <label class="color-scheme">
     Theme:
     <select id="color-scheme-select">
-      <option value="auto">Automatic</option>
+      <option value="light dark">Automatic</option>
       <option value="light">Light</option>
       <option value="dark">Dark</option>
     </select>
@@ -62,10 +62,10 @@ document.body.insertAdjacentHTML(
   `
 );
 
+
 let select = document.querySelector("#color-scheme-select");
 
 select.addEventListener("input", function (event) {
   console.log("color scheme changed to", event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
 });
-
-document.documentElement.style.setProperty('color-scheme', event.target.value);
