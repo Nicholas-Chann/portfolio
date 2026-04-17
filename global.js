637ca3e5c182ddf257c97ee5dd2b939932c5d635
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-17 09:12:19 -0700

    adjust nav bar

--- global.js
+++ global.js
@@ -4,6 +4,12 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                 // local dev server
+    : "/portfolio/";      // your GitHub Pages repo name
+
+
 let pages = [
   { url: "", title: "Home" },
   { url: "projects/", title: "Projects" },
@@ -15,11 +21,15 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  // next step: create link and add it to nav
+    if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
+}
 
-  // Create link and add it to nav
+let url = p.url;
+let title = p.title;
+// next step: create link and add it to nav
+
+// Create link and add it to nav
 nav.insertAdjacentHTML('beforeend', `<a href="/portfolio/${url}">${title}</a>`);
 }
 
