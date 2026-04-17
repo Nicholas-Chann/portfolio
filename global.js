5e55fd7535fd42d858990e98884b95055010d3d8
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-17 09:32:10 -0700

    Update global.js

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ let pages = [
   { url: "projects/", title: "Projects" },
   { url: "contacts/", title: "Contact" },
   { url: "Resume/", title: "Resume" },
-  {url: "https://github.com/nicholas-chann", title: "GitHub" }
+  {url: "https://github.com/Nicholas-Chann", title: "GitHub" }
 ];
 
 let nav = document.createElement('nav');
