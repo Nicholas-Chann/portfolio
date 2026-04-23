e529a02c626711318342af8d442553a9224c4bd1
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-23 09:39:52 -0700

    fixes

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
@@ -7,19 +8,20 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
-    
-  // Your code will go here
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+
   containerElement.innerHTML = '';
 
-  const article = document.createElement('article');
+  for (let project of projects) {
 
-  article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-`;
+    const article = document.createElement('article');
 
-containerElement.appendChild(article);
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
 
+    containerElement.appendChild(article);
+  }
 }
