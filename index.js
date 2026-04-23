7968cfeb238aca54d388535dd6e7bf56153a5c9e
Nicholas Chan <nicholaschan0813@gmail.com>
2026-04-23 12:33:32 -0700

    updated

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { fetchJSON, renderProjects, fetchGithubData } from '/portfolio/global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from '/portfolio/global.js';
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
-const githubData = await fetchGithubData('Nicholas-Chann');
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('Nicholas-Chann');
 const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
-        <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
-        </dl>
-    `;
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
 }
-
-renderProjects(latestProjects, projectsContainer, 'h2');
